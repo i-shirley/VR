d6a66c4210fa520d0a3eb344fbb3824b9e6143de
i-shirley <newswang2009@gmail.com>
2024-11-01 11:30:59 +0800

    96

---
+++ zoo/動物園門口/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "門口2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.8477038205456005,
+        "pitch": -0.03854598891961558,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.054638243969448,
+          "pitch": 0.1598051009731538,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "動物園門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6372625029824484,
+        "pitch": 0.10765350954029529,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07602557092363149,
+          "pitch": 0.6966856182699921,
+          "rotation": 3.141592653589793,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7610484107161124,
+          "pitch": 0.3185797963421795,
+          "title": "紅鶴區",
+          "text": "外型引人注目,屬於鳥類"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
